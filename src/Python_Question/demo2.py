e1a7a3552a629c86ad20494dcdf2106412b4fe3e
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2024-01-24 10:38:07 +0530

    This is the file for find 2nd largest no of sublist

--- src/Python_Question/demo2.py
+++ src/Python_Question/demo2.py
@@ -1,7 +1,7 @@
-input_data = [[1, 2, 3], [2, 4, 1, 0]]
+input = [[1, 2, 3], [2, 4, 1, 0]]
 
 # Calculate the sum of the second-largest number in each sub-list
-sum_second_largest = sum([sorted(sublist)[-3] for sublist in input_data])
+sum_second_largest = sum([sorted(sublist)[-2] for sublist in input])
 
 print("Sum of the second-largest numbers:", sum_second_largest)
 
